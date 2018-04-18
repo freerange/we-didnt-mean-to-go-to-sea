ae4e4c7507ebfa88f77e4ace37a9f51c8cc81c83
Ben Griffiths <bengriffiths@gmail.com>
2018-04-18 15:10:43 +0100

    (feature) extract text responder. Make sound more physical

--- game.rb
+++ game.rb
@@ -14,7 +14,8 @@ class Feature
   end
 
   def volume_from(x, y)
-    @volume * distance_from(x, y) 
+    distance = distance_from(x, y) + 1 
+    @volume / (distance * distance)
   end
 end
 
@@ -23,8 +24,8 @@ class Map
     @features = features
   end
 
-  def features_audible_from(x, y)
-    @features.map {|f| [f, f.volume_from(x, y)] }.select {|(_,fv)| fv < 3 } 
+  def features_audible_from(x, y, threshold = 0)
+    @features.map {|f| [f, f.volume_from(x, y)] }.select {|(_,fv)| fv > threshold }.shuffle 
   end
 end
 
@@ -51,39 +52,27 @@ class State
   end
 end
 
-class Game
-  def initialize(map)
-    @map = map
-  end
-  
+class TextResponder
   ACK = "Aye aye, Captain"
-  def step(state, action)
-    case action
-    when :go_north
-      [ACK, state.move(0, 1) ] 
-    when :go_south
-      [ACK, state.move(0, -1) ] 
-    when :go_west
-      [ACK, state.move(-1, 0) ] 
-    when :go_east
-      [ACK, state.move(1, 0) ] 
-    when :listen
-      [sounds_for_state(@map, state), state]
-    end
+
+  def success(state)
+    [ACK, state]
   end
 
-  def sounds_for_state(map, state)
-    audible_features = state.audible_features(map)
-    "You can hear: "+audible_features.map {|(f,fv)| f.name + " (" + adverb(fv) + ")" }.join(",")
+  def listen(features_and_volume, state)
+    response = "You can hear: "+features_and_volume.map {|(f,fv)| f.name + " (" + adverb(fv) + ")" }.join(",")
+    [response, state]
   end
-
+    
   def adverb(volume)
     case volume
-    when 0...1
+    when 1
+      "ear-splittingly loud"
+    when 0.5..1 
       "very loudly"
-    when 1...2
+    when 0.25...0.5 
       "loudly"
-    when 2...3
+    when 0.11...0.25
       "in the distance"
     else
       "barely"
@@ -91,17 +80,33 @@ class Game
   end
 end
 
+class Game
+  def initialize(map, responder)
+    @map = map
+    @responder = responder
+  end
+  
+  def step(state, action)
+    case action
+    when :go_north
+      @responder.success(state.move(0, 1)) 
+    when :go_south
+      @responder.success(state.move(0, -1)) 
+    when :go_west
+      @responder.success(state.move(-1, 0)) 
+    when :go_east
+      @responder.success(state.move(1, 0)) 
+    when :listen
+      @responder.listen(state.audible_features(@map), state)
+    end
+  end
+
+end
+
 if __FILE__ == $0
 
-  features = [
-    Feature.new('Buoy', 7, 7, 1),
-    Feature.new('Buoy', 7, 4, 1),
-    Feature.new('Buoy', 3, 2, 1),
-    Feature.new('Buoy', 4, 6, 1),
-    Feature.new('Lighthouse', 4, 8, 2)
-  ]
   map = Map.new(features)
-  game = Game.new(map)
+  game = Game.new(map, TextResponder.new)
 
   state = State.new(Random.rand(5)+3, Random.rand(5)+3)
   while true do
