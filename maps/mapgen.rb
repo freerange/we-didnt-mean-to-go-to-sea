5699d57d7b897ec04437bc16b0369601900f54c3
Ben Griffiths <bengriffiths@gmail.com>
2018-04-19 22:40:28 +0100

    (refactor) move code around to make it easy to replace ChunkyPNG

--- maps/mapgen.rb
+++ maps/mapgen.rb
@@ -11,15 +11,6 @@ MAP = ['LLLLLLLC~~',
        'CC~B~~CCLC',
        '~~~~~B~CC~']
 
-map_width = MAP.first.length
-map_height = MAP.length
-
-width = 500
-height = 500
-number_of_points = 1000
-
-voronoi = Voronoi.new(number_of_points, width, height)
-
 def type_of_map_point(x, y, width, height, map_width, map_height)
   map_x = x / (width / map_width)
   map_y = y / (height / map_height)
@@ -36,16 +27,59 @@ def type_of_map_point(x, y, width, height, map_width, map_height)
   end
 end
 
-voronoi.polygons.each do |p|
-  type = type_of_map_point(p.center.x, p.center.y, width, height, map_width, map_height)
-  p.annotations[:tile_type] = type
-  p.edges.each do |e|
-    if !e.annotations[:tile_types]
-      e.annotations[:tile_types] = []
+def annotate_polygons_with_tile_types(polygons, width, height, map_width, map_height)
+  polygons.each do |p|
+    type = type_of_map_point(p.center.x, p.center.y, width, height, map_width, map_height)
+    p.annotations[:tile_type] = type
+    p.edges.each do |e|
+      if !e.annotations[:tile_types]
+        e.annotations[:tile_types] = []
+      end
+      e.annotations[:tile_types] = (e.annotations[:tile_types] + [type]).uniq.sort
     end
-    e.annotations[:tile_types] = (e.annotations[:tile_types] + [type]).uniq.sort
   end
 end
+
+def each_triangle_with_tile_type(polygons)
+  polygons.each do |p|
+    type = p.annotations[:tile_type]
+    p.edges.each do |e|
+      triangle = [[e.v1.x, e.v1.y], [e.v2.x, e.v2.y], [p.center.x, p.center.y]]
+      yield type, triangle
+    end
+  end
+end
+
+def each_coastline_edge(edges)
+  edges.each do |e|
+    types = e.annotations[:tile_types]
+    if types == %i[coastline sea] || types == %i[land sea]
+      yield [e.v1.x, e.v1.y], [e.v2.x, e.v2.y]
+    end
+  end
+end
+
+def each_grid_center(width, height, map_width, map_height)
+  cell_width = width / map_width.to_f
+  cell_height = height / map_height.to_f
+  width.times do |x|
+    height.times do |y|
+      rx = x * cell_width + cell_width / 2.0
+      ry = y * cell_height + cell_height / 2.0
+      yield rx.to_i, ry.to_i
+    end
+  end
+end
+
+map_width = MAP.first.length
+map_height = MAP.length
+
+width = 300
+height = 300
+number_of_points = 400
+
+voronoi = Voronoi.new(number_of_points, width, height)
+annotate_polygons_with_tile_types(voronoi.polygons, width, height, map_width, map_height)
 
 require 'chunky_png'
 colors = {
@@ -57,29 +91,18 @@ colors = {
 }
 
 png = ChunkyPNG::Image.new(width, height, colors[:sea])
-voronoi.polygons.each do |p|
-  color = colors[p.annotations[:tile_type]]
-  p.edges.each do |e|
-    triangle = [[e.v1.x, e.v1.y], [e.v2.x, e.v2.y], [p.center.x, p.center.y], [e.v1.x, e.v1.y]]
-    png.polygon(triangle, color, color)
-  end
+
+each_triangle_with_tile_type(voronoi.polygons) do |type, triangle|
+  color = colors[type]
+  png.polygon(triangle, color, color)
 end
 
-voronoi.edges.each do |e|
-  types = e.annotations[:tile_types]
-  if types == %i[coastline sea]
-    png.line(e.v1.x.to_i, e.v1.y.to_i, e.v2.x.to_i, e.v2.y.to_i, colors[:coastlineline])
-  end
+each_coastline_edge(voronoi.edges) do |(x1,y1),(x2,y2)|
+  png.line(x1.to_i, y1.to_i, x2.to_i, y2.to_i, colors[:coastlineline])
 end
 
-cell_width = width / map_width
-cell_height = height / map_height
-width.times do |x|
-  height.times do |y|
-    rx = x * cell_width
-    ry = y * cell_height
-    png.rect(rx, ry, rx + 2, ry + 2, colors[:coastline], colors[:coastline])
-  end
+each_grid_center(width, height, map_width, map_height) do |rx,ry|
+  png.rect(rx, ry, rx + 2, ry + 2, colors[:coastline], colors[:coastline])
 end
 
 png.save('map.png', interlace: true)
